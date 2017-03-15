8d6820e2d6923cf5ca038abd39da8f42793d9ad7
JOHNKYON <zhaofengbo1321@163.com>
2017-03-15 13:18:16 +0800

    Github account test

--- Django/urls.py
+++ Django/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+# here's a test of github account
 from django.conf.urls import url
 from django.contrib import admin
 
